5eabad73547ff7f13ad26c1d741fb472ebe25969
Tianle Liu <40735546+Tianlel@users.noreply.github.com>
2021-03-29 13:22:48 -0500

    Create motor.py

---
+++ motor.py
@@ -0,0 +1,24 @@
+class KCubeDCMotorSpyrelet(Spyrelet):
+    requires = {
+        'motor': KCubeDCMotor,
+    }
+
+    @Task()
+    def goto_position(self):
+        params = self.parameters.widget.get()
+        self.motor.position = params['Position']
+        return
+
+    @Element()
+    def parameters(self):
+        params = [
+                ('Position', {
+                    'type': float,
+                    'bounds': [-180, 180],
+                    'default': 10,
+                    }),
+                ]
+        w = ParamWidget(params)
+        return w
+
+
